21501d6105d9e9e7874743e9f7f08e487245955e
Gabor Haraszti <GaborHaraszti@kuka-robotics.hu>
2017-10-25 08:59:04 +0200

    Resharpers additionsa

--- Billiard3D/Track/Room.cs
+++ Billiard3D/Track/Room.cs
@@ -1,47 +1,38 @@
-﻿using Billiard3D.Math;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
+using Billiard3D.Math;
 
 namespace Billiard3D.Track
 {
     internal class Room
     {
-        private double _roomLenght;
-        private double _roomWidth;
-        private double _roomHeight;
-
-        public List<Vector3D> Corners { get; set; } = new List<Vector3D>();
+        private readonly double _roomHeight;
+        private readonly double _roomLenght;
+        private readonly double _roomWidth;
 
         public Room(List<Vector3D> corners)
         {
             if (corners.Count != 8)
-            {
                 throw new ArgumentException($"Not enough points to constructs a room {nameof(corners)}");
-            }
             Corners.AddRange(corners);
             _roomLenght = Corners.Max(x => x.X) - Corners.Min(x => x.X);
             _roomWidth = Corners.Max(x => x.Y) - Corners.Min(x => x.Y);
             _roomHeight = Corners.Max(x => x.Z) - Corners.Min(x => x.Z);
         }
 
+        public List<Vector3D> Corners { get; set; } = new List<Vector3D>();
+
         public void StartSimulation(Vector3D startingPoint, Vector3D startingDirection)
         {
             var actualPosition = startingDirection + startingDirection;
-            while(InRoom(actualPosition))
+            while (InRoom(actualPosition))
             {
                 actualPosition += startingDirection;
             }
         }
 
-        private bool InRoom(Vector3D vector)
-        {
-            if (vector.X < _roomLenght && vector.Y < _roomWidth && vector.Z < _roomHeight)
-            {
-                return true;
-            }
-            return false;
-        }
+        private bool InRoom(Vector3D vector) =>
+            (vector.X < _roomLenght) && (vector.Y < _roomWidth) && (vector.Z < _roomHeight);
     }
 }
