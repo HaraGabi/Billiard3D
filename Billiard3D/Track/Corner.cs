95fc9a6bd166b5f4760326123b9d89eb7fc1f67e
Gabor Haraszti <GaborHaraszti@kuka-robotics.hu>
2017-11-20 19:10:21 +0100

    rgrgrg

---
+++ Billiard3D/Track/Corner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Billiard3D.VectorMath;
+using JetBrains.Annotations;
+
+namespace Billiard3D.Track
+{
+    internal class Corner
+    {
+        public Corner([NotNull] Vector3D cornerPoint) =>
+            CornerPoint = cornerPoint ?? throw new ArgumentNullException(nameof(cornerPoint));
+
+        public Corner([NotNull] Vector3D cornerPoint, double r)
+            : this(cornerPoint)
+        {
+        }
+
+        [NotNull]
+        public Vector3D CornerPoint { get; }
+    }
+}
